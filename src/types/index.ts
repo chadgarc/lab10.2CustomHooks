ccf9da0ac74e66acf9e55b5d47039c2144040d86
Christian Garlen <chadgarval@outlook.com>
2026-09-09 19:30:34 -0400

    Added data types

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+
+export interface ShowItemsProps {
+    itemList: string[];
+}
+
+export interface ShowItemsProps {
+    itemList: string[];
+}
+
+export interface ControlListProps {
+    items: string[];
+    itemsPerPage: number;
+}
+
+export interface PagesSetterProps{
+    onChange: (value: string) => void
+}
