8fe25b788b9baf96a060a422e55010d84b823982
Christian Garlen <chadgarval@outlook.com>
2026-09-12 01:57:58 -0400

    feat: implement debounce search functionality in DebounceDemo

--- src/components/Sections/DebouceDemo.tsx
+++ src/components/Sections/DebouceDemo.tsx
@@ -1,8 +1,56 @@
+import { useState } from 'react';
+import { useDebounce } from '../../hooks/useDebounce';
+import { useDataContext } from '../../context/DataContext';
 
 export function DebounceDemo() {
+
+    const { data } = useDataContext();
+
+    const [searchDelay, setSearchDelay] = useState(500);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const deboucedValue = useDebounce(searchTerm.trim(), searchDelay);
+    
     return (
         <div>
-            <h2>Debounce Demo</h2>
+            {/* This is from DaisyUI, it's a mock browser added for design purposes only, it does not do anything */}
+            <div className="mockup-browser bg-base-100 w-130 mx-auto mt-5 px-5 pb-5 border border-base-300">
+                <div className="mockup-browser-toolbar">
+                </div>
+                <div className="flex flex-col w-full">
+                    <h2>Debounce Search Demo</h2>
+                    <div className="flex justify-start items-center gap-2 mb-4 ms-10">
+                        <p>Debounce Delay (ms):</p>
+                        <input
+                            type="number"
+                            value={searchDelay}
+                            onChange={(e) => setSearchDelay(Number(e.target.value))}
+                            className="input input-bordered w-20 bg-gray-400 text-black max-w-xs"
+                        />
+                    </div>
+                    <div className='flex flex-col justify-start gap-4 text-left'>
+                        <input
+                            type="text"
+                            placeholder="Start typing..."
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                            className="input input-bordered bg-gray-400 text-black w-full"
+                        />
+                        {deboucedValue !== searchTerm ? <p>Typing...</p> : <p>Waiting for input...</p>}
+                        <p>Current Value: {searchTerm}</p>
+                        <p>Debounced Value (After {searchDelay}ms): {deboucedValue}</p>
+                        {deboucedValue.length <= 0 ? <p>Type to see results</p>: <div className='h-100 overflow-y-scroll'>
+                            {data.map((item, index) => {
+                                if (item.toLowerCase().includes(deboucedValue.toLowerCase())) {
+                                    return (
+                                        <div key={index}>{`${index + 1}. ${item}`}</div>
+                                    )
+                                }
+                            })}
+                        </div>}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }  
--- src/components/Sections/PaginationDemo.tsx
+++ src/components/Sections/PaginationDemo.tsx
@@ -3,16 +3,16 @@ import { PagesSetter } from "../PagesSetter/PagesSetter";
 import { useDataContext } from "../../context/DataContext";
 
 export function PaginationDemo() {
-  const { data, itemsPerPage } = useDataContext();
+    const { data, itemsPerPage } = useDataContext();
 
-  return (
-    <div className="w-130 mx-auto mt-5">
-      <h2>Pagination Demo</h2>
-      <section className="flex justify-between items-center my-5">
-        <PagesSetter />
-        <p>Total Items: {data.length}</p>
-      </section>
-      <ControlList items={data} itemsPerPage={itemsPerPage} />
-    </div>
-  );
+    return (
+        <div className="w-130 mx-auto mt-5">
+        <h2>Pagination Demo</h2>
+        <section className="flex justify-between items-center my-5">
+            <PagesSetter />
+            <p>Total Items: {data.length}</p>
+        </section>
+        <ControlList items={data} itemsPerPage={itemsPerPage} />
+        </div>
+    );
 }
